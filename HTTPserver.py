795a7505b1fa37fdca50134f28f12c2dd4258895
Kirthan <kirthan.cs21@bmsce.ac.in>
2023-04-18 18:32:56 +0530

    project done

---
+++ HTTPserver.py
@@ -0,0 +1,4 @@
+# (shift + right click and choose open windows powershell) wherever u want to create server and there is html there.
+# and write "python -m http.server --bind=localhost"
+# and go to any browser and search "localhost:8000"
+# what is jQuery?
